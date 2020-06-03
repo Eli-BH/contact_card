19d9b501a9e7135ea003cce0afc13ca2eb3826db
Eli-b-good <eli.i.henderson95@gmail.com>
2020-06-02 23:58:18 -0400

    added Contact Card class

---
+++ src/ContactCard.js
@@ -0,0 +1,18 @@
+import React, {component} from "react"; 
+import PropTypes from "prop-types";
+
+class ContactCard extends Component{
+    render(){
+        return(
+            <div>
+                <h1>{this.props.name}</h1>
+                <ul>
+                    <li>mobile: {this.props.mobile}</li>
+                    <li>work: {this.props.work}</li>
+                    <li>email: {this.props.email}</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
